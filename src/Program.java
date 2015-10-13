b63e8d43c54cdb91e890d40611abe131c592500a
Dmitry Makhnev <dmitry.ivshin@gmail.com>
2015-10-13 11:31:21 +0300

    HW 1

---
+++ src/Program.java
@@ -0,0 +1,16 @@
+/**
+ * $Description$
+ *
+ * @author <a href="mailto:dmitry.makhnev@corp.mail.ru">Dmitry Makhnev</a>
+ */
+public class Program {
+
+    public static void main(String[] arguments) {
+        System.out.print(floatDivision(9, 10));
+    }
+
+    private static float floatDivision(int a, int b) {
+        return (float) a / b;
+    }
+
+}
